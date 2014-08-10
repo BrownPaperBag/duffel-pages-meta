a3f3f82995d6660cc01eee96ebaa8645500ca35b
Michael Robinson <mike@pagesofinterest.net>
2014-08-10 19:57:47 +1200

    Ouptut meta default model

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ module.exports = {
   initialise: require('./lib/initialise'),
   Meta: function() {
     return require('./lib/models/Meta').model();
+  },
+  MetaDefault: function() {
+    return require('./lib/models/MetaDefault').model();
   }
 };
